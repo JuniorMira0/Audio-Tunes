de27c084ca2e4e0f9f4ee83afa6111d81633f4b7
JuniorMira <j-unyor14@hotmail.com>
2022-03-15 13:51:48 -0300

    requisito 13

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -4,7 +4,7 @@ class NotFound extends React.Component {
   render() {
     return (
       <div data-testid="page-not-found">
-        <h1> Not found </h1>
+        Not found
       </div>
     );
   }
--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,11 +1,58 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Cabeçalho/Header';
+import Loading from '../components/Loading/Loading';
+import { getUser } from '../services/userAPI';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      email: '',
+      image: '',
+      description: '',
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.getUserLogin();
+  }
+
+  getUserLogin = async () => {
+    const user = await getUser();
+    this.setState({
+      name: user.name,
+      email: user.email,
+      image: user.image,
+      description: user.description,
+      loading: true,
+    });
+  }
+
   render() {
+    const {
+      name,
+      email,
+      image,
+      description,
+      loading,
+    } = this.state;
+
     return (
       <div data-testid="page-profile">
         <Header />
+        {loading ? <Loading /> : (
+          <div>
+            <img data-testid="profile-image" src={ image } alt={ name } />
+            <span>{ name }</span>
+            <span>{ email }</span>
+            <span>{ description }</span>
+          </div>
+        )}
+        <Link to="/profile/edit">Editar perfil</Link>
       </div>
     );
   }
